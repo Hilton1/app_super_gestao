22b6e7027dbed4b72f17cf206ded4ec4a8bf040c
Hilton1 <josehilton02@gmail.com>
2023-08-07 10:42:29 -0300

    Adicionando validação "required" no form

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContato;
 
 class ContatoController extends Controller
 {
-    public function contato() {
+  public function contato(Request $request) {
+    return view('site.contato', ['titulo' => 'Contato']);
+  }
 
-        // var_dump($_POST);
-        return view('site.contato', ['titulo' => 'Contato']);
-    }
+  public function salvar(Request $request) {
+    // SiteContato::create($request->all());
+    $request->validate([
+      'nome' => 'required',
+      'telefone' => 'required',
+      'email' => 'required',
+      'motivo_contato' => 'required',
+      'mensagem' => 'required',
+    ]);
+  }
 }
