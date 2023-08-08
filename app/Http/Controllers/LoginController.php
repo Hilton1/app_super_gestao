cc79805861311e559873ac24a77b0fa1aa264c69
Hilton1 <josehilton02@gmail.com>
2023-08-08 14:49:57 -0300

    Validando usuário no banco

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class LoginController extends Controller
 {
@@ -25,6 +26,25 @@ class LoginController extends Controller
 
     $request->validate($regras, $feedback);
 
-    print_r($request->all());
+    // Recuperando os parâmetros do usuário
+    $email = $request->get('usuario');
+    $password = $request->get('senha');
+
+    echo "Usuário: $email - Senha: $password";
+    echo '<br>';
+
+    // Iniciando o Model User
+    $user = new User();
+
+    $usuario = $user->where('email', $email)
+      ->where('password', $password)
+      ->get()
+      ->first();
+
+    if(isset($usuario->name)) {
+      echo 'Usuário existe';
+    } else {
+      echo 'Usuário não existe.';
+    }
   }
 }
