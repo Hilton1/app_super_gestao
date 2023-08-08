58dfc0a3c2c4fb051c69b06466e8f164cffb187b
Hilton1 <josehilton02@gmail.com>
2023-08-07 23:32:18 -0300

    Customizando as mensagens de erro

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -16,13 +16,27 @@ class ContatoController extends Controller
   }
 
   public function salvar(Request $request) {
-    $request->validate([
+    $regras = [
       'nome' => 'required|min:3|max:40|unique:site_contatos',
       'telefone' => 'required',
       'email' => 'email',
       'motivo_contatos_id' => 'required',
-      'mensagem' => 'required',
-    ]);
+      'mensagem' => 'required|max:2000',
+    ];
+
+    $feedback = [
+      'nome.min' => 'O campo nome precisa ter, no mínimo, 3 caracteres.',
+      'nome.max' => 'O campo nome precisa ter, no máximo, 40 caracteres.',
+      'nome.unique' => 'O nome informado já está em uso.',
+
+      'email.email' => 'Você deve inserir um e-mail válido.',
+
+      'mensagem.max' => 'A mensagem deve ter, no máximo, 2000 caracteres.',
+
+      'required' => 'O campo :attribute deve ser preenchido.'
+    ];
+
+    $request->validate($regras, $feedback);
 
     SiteContato::create($request->all());
     return redirect()->route('site.index');
