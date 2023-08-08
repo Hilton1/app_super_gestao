4db724e26f0696d76359b43beeac27720142d496
Hilton1 <josehilton02@gmail.com>
2023-08-08 00:10:40 -0300

    Implementando middleware de Log de Acessos

--- app/Http/Middleware/LogAcessoMiddleware.php
+++ app/Http/Middleware/LogAcessoMiddleware.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use App\LogAcesso;
 
 class LogAcessoMiddleware
 {
@@ -16,6 +17,12 @@ class LogAcessoMiddleware
   public function handle($request, Closure $next)
   {
     // return $next($request);
+    // dd($request);
+
+    $ip = $request->server->get('REMOTE_ADDR');
+    $rota = $request->getRequestUri();
+    LogAcesso::create(['log' => "IP $ip requisitou a rota $rota"]);
+
     return Response('Chegamos no middleware e finalizamos no próprio middleware.');
   }
 }
