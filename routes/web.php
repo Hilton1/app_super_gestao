c26438e946f7ecc5d399d437899fe428aa939ce0
Hilton1 <josehilton02@gmail.com>
2023-08-10 09:43:06 -0300

    Adicionando views de fornecedores

--- routes/web.php
+++ routes/web.php
@@ -25,8 +25,14 @@ Route::middleware('autenticacao:padrao,visitante')->prefix('/app')->group(functi
   Route::get('/home', 'HomeController@index')->name('app.home');
   Route::get('/sair', 'LoginController@sair')->name('app.sair');
   Route::get('/cliente', 'ClienteController@index')->name('app.cliente');
-  Route::get('/fornecedor', 'FornecedorController@index')->name('app.fornecedor');
   Route::get('/produto', 'ProdutoController@index')->name('app.produto');
+
+  Route::get('/fornecedor', 'FornecedorController@index')->name('app.fornecedor');
+  Route::get('/fornecedor/adicionar', 'FornecedorController@adicionar')
+    ->name('app.fornecedor.adicionar');
+  Route::post('/fornecedor/listar', 'FornecedorController@listar')
+    ->name('app.fornecedor.listar');
+
 });
 
 Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('site.teste');
